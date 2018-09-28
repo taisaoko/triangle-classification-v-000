43ab0d67c122f0adc953af206b9bde19a6077874
taisaoko <ttd3calvin@gmail.com>
2018-09-28 22:16:34 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,9 @@
 class Triangle
   # write code here
   def initialize (side_1, side_2, side_3)
-    @length_1 = length_1
-    @length_2 = length_2
-    @length_3 = length_3
+    
   end
   
   def kind
+    
 end
